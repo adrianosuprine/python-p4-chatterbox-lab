1b31797b5560ad6764f2223561b1d9e8736cb1a4
adrianosuprine <adrianosuprine@gmail.com>
2024-04-14 19:08:35 +0300

    added files

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from sqlalchemy import asc
 
 from models import db, Message
 
@@ -14,13 +15,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_serialized = [messages.to_dict() for messages in messages]
+        response = jsonify(messages_serialized),200
+        return response
+    elif request.method == 'POST':
+        body = request.json.get('body')
+        username = request.json.get('username')
+        
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+        
+        response = jsonify(new_message.to_dict()),201
+        return response 
+    
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'error':'message not found'}), 404
+    if 'body' in request.json:
+        message.body = request.json['body']
+    db.session.commit()
+    return jsonify(message.to_dict())
 
-@app.route('/messages/<int:id>')
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    db.session.delete(message)
+    db.session.commit()
+        
+    return jsonify({'message': 'message delete successfully'}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
